34d1f51075f0942576edbc991efa31586cf51fc5
Alex Pritchard <alex.pritchard@soartech.com>
2013-04-19 14:02:22 -0400

    Shell projet

---
+++ simple-java-wunderground-api/src/main/java/com/amp/casual/simple_java_wunderground_api/App.java
@@ -0,0 +1,13 @@
+package com.amp.casual.simple_java_wunderground_api;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
